0a27e216a3b83d0d73e75e5915486b40cf87d409
Daniel <naturforscher@outlook.com>
2022-10-10 19:21:46 +0300

    Implemented JOBS

---
+++ Assets/_Scripts/SpawnerParallel.MoveJob.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Jobs;
+
+
+    public struct MoveJob: IJobParallelForTransform
+    {
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += .1f * (transform.rotation * new Vector3(0,0,1));
+         
+            if (transform.position.z > 50)
+                transform.position = 
+                    new Vector3(transform.position.x, 0, -50);
+        }
+    }
--- Assets/_Scripts/SpawnerParallel.cs
+++ Assets/_Scripts/SpawnerParallel.cs
@@ -1,34 +1,57 @@
 using System;
-using System.Collections.Generic;
+using Unity.Jobs;
 using UnityEngine;
-using Random = UnityEngine.Random;
 using UnityEngine.Jobs;
+using Random = UnityEngine.Random;
 
 public class SpawnerParallel : MonoBehaviour
 {
    [SerializeField] private GameObject _sheepPrefab;
    [SerializeField] private int _spawnAmount;
-   private GameObject[] _sheepList;
+   private Transform[] _sheepTransforms;
    
    [SerializeField] private float _sheepSpeed;
+
+   private MoveJob _moveJob;
+   private JobHandle _moveHandle;
+   private TransformAccessArray _transforms;
+
    private void Start()
    {
-      _sheepList = new GameObject[_spawnAmount];
+      _sheepTransforms = new Transform[_spawnAmount];
       for (int i = 0; i < _spawnAmount; i++)
       {
-          _sheepList[i] = Instantiate(_sheepPrefab, 
+          var sheep = Instantiate(_sheepPrefab, 
             new Vector3(Random.Range(-50,50),0, Random.Range(-50,50)), Quaternion.identity);
+          _sheepTransforms[i] = sheep.transform;
       }
+
+      _transforms = new TransformAccessArray(_sheepTransforms);
    }
 
    private void Update()
    {
-      for (int i = 0; i < _sheepList.Length; i++)
+      var moveJob = new MoveJob();
+      _moveHandle = moveJob.Schedule(_transforms);
+   }
+
+   private void LateUpdate()
+   {
+      _moveHandle.Complete();
+   }
+
+   private void OldUnparallelProcess()
+   {
+      for (int i = 0; i < _sheepTransforms.Length; i++)
       {
-         _sheepList[i].transform.Translate(0,0,_sheepSpeed);
-         if (_sheepList[i].transform.position.z > 50)
-            _sheepList[i].transform.position = 
-               new Vector3(_sheepList[i].transform.position.x, 0, -50);
+         _sheepTransforms[i].transform.Translate(0, 0, _sheepSpeed);
+         if (_sheepTransforms[i].transform.position.z > 50)
+            _sheepTransforms[i].transform.position = new Vector3(_sheepTransforms[i].transform.position.x, 0, -50);
       }
+   }
+
+   private void OnDestroy()
+   {
+      _transforms.Dispose();
    }
 }
